3a8b62ddf007c74084e8cdd66450bf46161094a4
Rafael Lima <rafauxdev@gmail.com>
2023-11-30 08:32:50 +0000

    Finish section 4

--- Section_4-Project_Exercise/pages/events/[...slug].js
+++ Section_4-Project_Exercise/pages/events/[...slug].js
@@ -1,5 +1,35 @@
+import { useRouter } from "next/router"
 import React from "react"
+import { getFilteredEvents } from "../../dummy-data"
+import EventList from "../../components/events/EventList"
 
 export default function FilteredEventsPage() {
-  return <div>FilteredEventsPage</div>
+  const router = useRouter()
+
+  const filterData = router.query.slug
+
+  if (!filterData) {
+    return <p className="center">Loading</p>
+  }
+
+  const filterYear = filterData[0]
+  const filterMonth = filterData[1]
+  const numYear = +filterYear
+  const numMonth = +filterMonth
+
+  if (isNaN(numYear) || isNaN(numMonth)) {
+    return <p>Invalid filter</p>
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth })
+
+  if (!filteredEvents || !filteredEvents.length) {
+    return <p>No events founds.</p>
+  }
+
+  return (
+    <div>
+      <EventList items={filteredEvents} />
+    </div>
+  )
 }
--- Section_4-Project_Exercise/pages/events/index.js
+++ Section_4-Project_Exercise/pages/events/index.js
@@ -2,13 +2,20 @@ import React from "react"
 import { getAllEvents } from "../../dummy-data"
 import EventList from "../../components/events/EventList"
 import EventsSearch from "../../components/events/EventsSearch"
+import { useRouter } from "next/router"
 
 export default function EventsPage() {
   const events = getAllEvents()
+  const router = useRouter()
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath)
+  }
   return (
     <div>
       <h1>EventsPage</h1>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </div>
   )
