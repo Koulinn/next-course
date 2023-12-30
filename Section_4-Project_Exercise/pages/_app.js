bf78bd09fbb70aec7d108b86cf7d2cbe2b33781d
Rafael Lima <rafauxdev@gmail.com>
2023-12-30 07:35:01 +0000

    Up to 216

--- Section_4-Project_Exercise/pages/_app.js
+++ Section_4-Project_Exercise/pages/_app.js
@@ -1,9 +1,21 @@
+import Head from "next/head"
 import Layout from "../components/layout/layout"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      {/* Next automatically merge heads */}
+      <Head>
+        <title>
+          General title shared across all pages that doesn't have a specific
+          data
+        </title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        ></meta>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
